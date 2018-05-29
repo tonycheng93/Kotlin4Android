4ba22f4fb2771349e356a12062b09dc19059b437
baocheng <baocheng@yy.com>
2018-05-29 17:52:45 +0800

    add base ui

---
+++ baselibrary/src/main/res/com.sky.baselibrary/ui/adapter/BaseAdapter.kt
@@ -0,0 +1,4 @@
+package com.sky.baselibrary.ui.adapter
+
+open class BaseAdapter {
+}
---
+++ baselibrary/src/main/res/com.sky.baselibrary/ui/fragment/BaseFragment.kt
@@ -0,0 +1,26 @@
+package com.sky.baselibrary.ui.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+open class BaseFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
